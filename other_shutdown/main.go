2f2991a38bc6bc96622ca2e1173abeb9cc80c693
linuxxiaoyu <linuxxiaoyu@gmail.com>
2020-05-10 17:48:27 +0800

    Add other_shutdown dir

---
+++ other_shutdown/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		time.Sleep(5 * time.Second)
+		c.String(200, "hello test")
+	})
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("ListenAndServe err:", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Server shutdown")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Shutdown err:", err)
+	}
+
+	log.Println("Server exit")
+}
